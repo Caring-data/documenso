b881c025b2db98980f562188b844af9c7ec52110
jtatianaortiz <13875825+jtatianaortiz@users.noreply.github.com>
2025-06-17 06:58:14 -0500

    fix: comments

--- packages/lib/constants/signatures.ts
+++ packages/lib/constants/signatures.ts
@@ -1,6 +1,5 @@
 export const SIGNATURE_CANVAS_DPI = 2;
 export const SIGNATURE_MIN_COVERAGE_THRESHOLD = 0.01;
 
-// export const isBase64Image = (value: string) => value.startsWith('data:image/png;base64,');
 export const isBase64Image = (value: unknown): value is string =>
   typeof value === 'string' && value.startsWith('data:image/png;base64,');
